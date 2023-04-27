7d75344b0ed16a5eb6fc3428d931c2302767e714
Jonathan Silva <jonalves.silva@jsmail.com.br>
2023-04-27 13:39:32 -0300

    Client: Modify App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,25 @@
 import React from "react";
+import { Appbar } from "./components/Appbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./routes/Home";
 import "./App.css";
+import { FuncionariosView } from "./routes/FuncionariosView";
+import { FornecedoresView } from "./routes/FornecedoresView";
 
 function App() {
-  return <div className="h-16 bg-blue-600"></div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Appbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/funcionarios" element={<FuncionariosView />} />
+          <Route path="/fornecedores" element={<FornecedoresView />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
